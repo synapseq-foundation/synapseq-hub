742ec9b68d6c1e50fb1db7192caa352c4b40b7c1
Ruan <ruan.klein@gmail.com>
2026-05-04 08:56:45 -0300

    fix: favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import './layout.css';
 
-	import favicon from '$lib/assets/favicon.svg';
 	import favicon32 from '$lib/assets/favicon-32.png';
 	import favicon48 from '$lib/assets/favicon-48.png';
 	import favicon96 from '$lib/assets/favicon-96.png';
@@ -14,7 +13,6 @@
 
 <svelte:head>
 	<link rel="icon" href={faviconIco} sizes="any" />
-	<link rel="icon" href={favicon} type="image/svg+xml" />
 	<link rel="icon" href={favicon32} sizes="32x32" type="image/png" />
 	<link rel="icon" href={favicon48} sizes="48x48" type="image/png" />
 	<link rel="icon" href={favicon96} sizes="96x96" type="image/png" />
